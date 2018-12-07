9b4b63cff7c952061296938b8bf94daaa692417a
zhenming <1119264845@qq.com>
2018-12-07 10:09:30 +0800

    feat interactive swagger (#6)
    * feat editBigQuestion
    
    * fix editBigQuestion type conversion exception
    
    * feat interactive ASK
    
    * feat interactive swagger

--- src/main/java/com/forteach/quiz/web/vo/RaisehandVo.java
+++ src/main/java/com/forteach/quiz/web/vo/RaisehandVo.java
@@ -1,5 +1,7 @@
 package com.forteach.quiz.web.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import static com.forteach.quiz.common.Dic.ASK_RAISE_HAND;
@@ -11,16 +13,19 @@ import static com.forteach.quiz.common.Dic.ASK_RAISE_HAND;
  * @date: 2018/12/4  15:02
  */
 @Data
+@ApiModel(value = "学生举手", description = "通过课堂圈子id 及 学生id举手")
 public class RaisehandVo {
 
     /**
      * 学生id
      */
+    @ApiModelProperty(value = "学生id", name = "examineeId")
     private String examineeId;
 
     /**
      * 课堂圈子id
      */
+    @ApiModelProperty(value = "课堂圈子id", name = "circleId")
     private String circleId;
 
 
