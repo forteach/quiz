718dbf9606ed68f9a4a16ff3471137a111246ca3
lzm <1119264845@qq.com>
2019-01-24 11:07:20 +0800

    feat team

---
+++ src/main/java/com/forteach/quiz/interaction/team/service/TeamService.java
@@ -0,0 +1,120 @@
+package com.forteach.quiz.interaction.team.service;
+
+import com.forteach.quiz.exceptions.CustomException;
+import com.forteach.quiz.interaction.execute.service.ClassRoomService;
+import com.forteach.quiz.interaction.team.web.vo.GroupRandomVo;
+import com.forteach.quiz.web.pojo.Students;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.redis.core.ReactiveHashOperations;
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+
+import static com.forteach.quiz.util.StringUtil.getRandomUUID;
+
+/**
+ * @Description:
+ * @author: liu zhenming
+ * @version: V1.0
+ * @date: 2019/1/22  15:48
+ */
+@Service
+public class TeamService {
+
+    private final ReactiveStringRedisTemplate stringRedisTemplate;
+    private final ReactiveHashOperations<String, String, String> reactiveHashOperations;
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
+    private final ClassRoomService classRoomService;
+
+    public TeamService(ReactiveStringRedisTemplate stringRedisTemplate,
+                       ReactiveHashOperations<String, String, String> reactiveHashOperations,
+                       ReactiveMongoTemplate reactiveMongoTemplate,
+                       ClassRoomService classRoomService) {
+        this.stringRedisTemplate = stringRedisTemplate;
+        this.reactiveHashOperations = reactiveHashOperations;
+        this.reactiveMongoTemplate = reactiveMongoTemplate;
+        this.classRoomService = classRoomService;
+    }
+
+    /**
+     * 随机分组
+     *
+     * @return
+     */
+    public Mono<Map> groupRandom(final Mono<GroupRandomVo> random) {
+
+        return random
+                .filterWhen(randomVo -> allotVerify(classRoomService.studentNumber(randomVo.getCircleId()), randomVo.getNumber()))
+                .flatMap(randomVo -> {
+
+                    Mono<List<Students>> list = classRoomService.findInteractiveStudents(randomVo.getCircleId()).transform(this::shuffle);
+
+
+                    return list.map(students -> {
+                        //总数 , 组数 , 每组个数 , 余数 ,余数累加值
+                        int size = students.size();
+                        int teamNumber = randomVo.getNumber();
+                        int nitems = size / teamNumber;
+                        int residue = size % teamNumber;
+
+                        Map<String, List<Students>> grouping = new HashMap<>(randomVo.getNumber());
+
+                        //截取分组
+                        for (int i = 0; i < teamNumber; i++) {
+
+                            List<Students> studentsList;
+//                            余数累加
+                            if (residue != 0) {
+                                studentsList = Collections.singletonList((students.get(students.size() - residue)));
+                                residue--;
+                            } else {
+                                studentsList = new ArrayList<>();
+                            }
+                            studentsList.addAll(students.subList(i * nitems, i * nitems + nitems));
+                            grouping.put(getRandomUUID(), studentsList);
+
+                        }
+
+                        return grouping;
+
+                    });
+                });
+    }
+
+
+    /**
+     * 至少每组两个人
+     *
+     * @param size
+     * @param number
+     * @return
+     */
+    private Mono<Boolean> allotVerify(final Mono<Long> size, final Integer number) {
+        return size
+                .map(count -> count > number * 2)
+                .map(flag -> {
+                    if (flag) {
+                        return flag;
+                    } else {
+                        throw new CustomException("分组时 至少需要条件达到每组两个人");
+                    }
+                });
+    }
+
+    /**
+     * 打乱学生列表顺序
+     *
+     * @param listMono
+     * @return
+     */
+    private Mono<List<Students>> shuffle(final Mono<List<Students>> listMono) {
+        return listMono
+                .map(list -> {
+                    Collections.shuffle(list);
+                    return list;
+                });
+    }
+
+}
---
+++ src/main/java/com/forteach/quiz/interaction/team/web/vo/GroupRandomVo.java
@@ -0,0 +1,27 @@
+package com.forteach.quiz.interaction.team.web.vo;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @Description:
+ * @author: liu zhenming
+ * @version: V1.0
+ * @date: 2019/1/23  11:00
+ */
+@Data
+public class GroupRandomVo {
+
+    /**
+     * 课堂圈子id
+     */
+    @ApiModelProperty(value = "课堂圈子id", name = "circleId")
+    private String circleId;
+
+    /**
+     * 共分几组
+     */
+    @ApiModelProperty(value = "要分分几个组", name = "number")
+    private Integer number;
+
+}
